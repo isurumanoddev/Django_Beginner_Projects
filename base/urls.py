0b48dcb661e1b2496a9cab175003f88d5f624dca
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-02-07 13:12:42 +0530

    Add user login and register

--- base/urls.py
+++ base/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('create_room/', views.create_room, name="create-room"),
     path('update_room/<str:pk>/', views.update_room, name="update-room"),
     path('delete_room/<str:pk>/', views.delete_room, name="delete-room"),
+    path('user_login/', views.user_login, name="login"),
+    path('user_logout/', views.user_logout, name="logout"),
+    path('user_register/', views.user_register, name="register"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from base.form import RoomForm
@@ -13,7 +18,7 @@ def home(request):
         print(q)
     else:
         q = ""
-    rooms= Room.objects.filter(topic__name__contains=q)
+    rooms = Room.objects.filter(topic__name__contains=q)
     topics = Topic.objects.all()
     context = {"rooms": rooms, "topics": topics}
     return render(request, "home.html", context)
@@ -26,6 +31,7 @@ def room(request, pk):
     return render(request, "room.html", context)
 
 
+@login_required(login_url="login")
 def create_room(request):
     form = RoomForm()
     if request.method == "POST":
@@ -38,9 +44,11 @@ def create_room(request):
     return render(request, "room_form.html", context)
 
 
+@login_required(login_url="login")
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
@@ -51,6 +59,7 @@ def update_room(request, pk):
     return render(request, "room_form.html", context)
 
 
+@login_required(login_url="login")
 def delete_room(request, pk):
     room = Room.objects.get(id=pk)
     if request.method == "POST":
@@ -58,3 +67,42 @@ def delete_room(request, pk):
         return redirect("home")
     context = {"room": room}
     return render(request, "delete.html", context)
+
+
+def user_login(request):
+    page = "login_page"
+    if request.user.is_authenticated:
+        return redirect("home")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+            print(username, password)
+
+        except:
+            print("user not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return HttpResponse("<h1>User Not Registered</h1>")
+    context = {"page": page}
+    return render(request, "login_register.html", context)
+
+
+def user_logout(request):
+    logout(request)
+
+    return redirect("home")
+
+
+def user_register(request):
+    form = UserCreationForm(
+    )
+    context = {"form": form}
+    return render(request, "login_register.html", context)
