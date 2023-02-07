c64c4698564329703445ed0b90c2cb631a5ae0be
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-02-07 08:38:23 +0530

    add home and room

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from base import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create_room/', views.create_room, name="create-room"),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from base.models import Room, Message, Topic
+
+
 # Create your views here.
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, "home.html", context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    room_comments = Message.objects.all()
+    topics = Topic.objects.all()
+    context = {"room": room, "room_comments": room_comments, "topics": topics}
+    return render(request, "room.html", context)
+
+
+def create_room(request):
+    if request.method == "POST":
+        form =
+    context = {"rooms": rooms}
+    return render(request, "home.html", context)
